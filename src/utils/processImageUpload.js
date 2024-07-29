e542dbabbacece91c834715f2d2a66c564971516
naira <yeghiazaryan.naira1@gmail.com>
2024-07-29 19:58:12 +0400

    feat(utils): Implement processImageUpload helper

---
+++ src/utils/processImageUpload.js
@@ -0,0 +1,22 @@
+/**
+ * Processes the image upload and sets the uploaded image to the video recorder.
+ *
+ * @param {Event} event - The event object triggered by the file input change.
+ * @param {Object} videoRecorder - The video recorder object.
+ */
+const processImageUpload = (event, videoRecorder) => {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const image = new Image();
+      image.onload = () => {
+        videoRecorder.uploadedImage = image;
+      };
+      image.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+};
+
+export default processImageUpload;
